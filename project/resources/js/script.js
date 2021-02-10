e6f1af29141f807be35294ddc5c019dbc896bdb0
Adeel Arif Bhatti <adeelarifbhatti@gmail.com>
2021-02-11 00:29:00 +0100

    learning javascript

---
+++ project/resources/js/script.js
@@ -0,0 +1,12 @@
+var var1, var2, var3;
+
+var1 = 4;
+var2 = 3;
+var3 = 4;
+
+document.getElementById("myBtn").addEventListener("click", function() {
+   var test2 = document.getElementById("div-one-id");
+    test2.style.backgroundColor= "red";
+    console.log("successful")
+  });
+
